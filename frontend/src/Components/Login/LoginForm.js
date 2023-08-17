9c29224a996ef760b0e578575ad4e9688cd990ba
Abdouallah <abdouallah.tahdi@mail.mcgill.ca>
2023-08-17 13:16:20 -0400

    Added Login form

---
+++ frontend/src/Components/Login/LoginForm.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './LoginForm.css';
+
+function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginError, setLoginError] = useState(null);
+  const [loginSuccess, setLoginSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('/api/login', { email, password });
+      console.log('Login successful:', response.data);
+      localStorage.setItem('jwtToken', response.data.token);
+      setLoginSuccess(true);
+      setLoginError(null);
+    } catch (error) {
+      console.error('Login error:', error);
+      setLoginError('Login failed. Please check your credentials.');
+      setLoginSuccess(false);
+    }
+  };
+
+  return (
+    <div className="login-form">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
+      </form>
+      {loginSuccess && <p>Login successful!</p>}
+      {loginError && <p>{loginError}</p>}
+    </div>
+  );
+}
+
+export default LoginForm;
